79b927e2b6165b92ae2521e46c5734e76ecbe961
jameswhitakerwork <jameswhitakerwork@gmail.com>
2017-05-10 19:43:32 +0800

    fixed settings

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -89,7 +89,7 @@ INSTALLED_APPS = [
 
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
